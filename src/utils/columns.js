91b9dff95374cde0789bed375387cc2439b4d9ff
taufik <taufik@codelabs.co.id>
2025-11-01 18:26:17 +0700

    feat: manage banner module

---
+++ src/utils/columns.js
@@ -0,0 +1,37 @@
+export const userListHeaders = [
+  // { label: 'Photo', key: 'photo' },
+  { label: 'Name', key: 'fullName' },
+  { label: 'Email', key: 'email' },
+  { label: 'Role', key: 'role' },
+  // { label: 'Status', key: 'status' },
+  { label: 'Actions', key: 'actions' },
+]
+
+export const rolesHeaders = [
+  { label: 'Role Name', key: 'RoleName' },
+  { label: 'Actions', key: 'actions' },
+]
+
+export const customerHeaders = [
+  { label: 'Photo', key: 'photo' },
+  { label: 'Name', key: 'name' },
+  { label: 'Email', key: 'email' },
+  { label: 'Phone', key: 'phone' },
+  { label: 'Status', key: 'status' },
+  { label: 'Actions', key: 'actions' },
+]
+
+export const bannerHeaders = [
+  { label: 'Image', key: 'image' },
+  { label: 'Name', key: 'name' },
+  { label: 'Type', key: 'type' },
+  { label: 'Periode', key: 'periode' },
+  { label: 'Actions', key: 'actions' },
+]
+
+export const walkthroughHeaders = [
+  { label: 'Image', key: 'image' },
+  { label: 'Name', key: 'name' },
+  { label: 'Type', key: 'type' },
+  { label: 'Actions', key: 'actions' },
+]
---
+++ src/views/banner/Banner.vue
@@ -0,0 +1,141 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { BaseButton, BaseTable, BaseInput, Drawer, PermissionGroup, BaseTitle, BaseAction, BaseSelect, BaseUpload, DateRangePicker } from '@/components'
+import { bannerHeaders } from "@/utils/columns"
+import { dataBanner } from "@/utils/data"
+import moment from "moment"
+import { toast } from "vue-sonner"
+
+const isLoading = ref(false)
+const isProcess = ref(false)
+const isAction = ref(false)
+const isEdit = ref(false)
+const isDrawerOpen = ref(false)
+const banners = ref([])
+const pages = ref({ current: 1, per: 10, last: 0, total: 0 })
+const form = ref({
+  id: null,
+  name: '',
+  image: '',
+  type: '',
+  startDate: '',
+  endDate: '',
+  dateRange: '',
+})
+
+const typeOptions = [
+  { label: 'Developer', value: 'developer' },
+  { label: 'Customer', value: 'customer' },
+]
+
+const fetchBanners = async (params) => {
+  try {
+    // const { data, meta } = await apiGetBanners(params)
+    const { data, meta } = dataBanner
+    banners.value = data
+    pages.value = {
+      current: meta.current_page,
+      per: meta.per_page,
+      last: meta.last_page,
+      total: meta.total
+    }
+  } catch (error) {
+    toast.error(error?.message || 'Failed to load banners')
+  } finally {
+    isProcess.value = false
+  }
+}
+
+onMounted(async () => {
+  isLoading.value = true
+  try {
+    await Promise.all([
+      fetchBanners({ page: pages.value.current, limit: pages.value.per })
+    ])
+  } finally {
+    isLoading.value = false
+  }
+})
+
+// Event handlers
+const handleEdit = (item) => console.log('Edit action:', item)
+const handleDelete = (item) => console.log('Delete action:', item)
+const handlePageChange = (page) => {
+
+}
+
+const handleSave = () => {
+  console.log('added:', form.value)
+  isDrawerOpen.value = false
+}
+
+const cancelForm = () => {
+  isDrawerOpen.value = false
+  form.value = {
+    name: '',
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <BaseTitle title="Manage Banner" />
+
+    <div class="flex items-center justify-between px-5 py-6 bg-white rounded-t-lg">
+      <h2 class="font-medium text-lg text-gray-800">Banner</h2>
+      <BaseButton @click="isDrawerOpen = true" variant="filled">
+        Add New
+      </BaseButton>
+    </div>
+
+    <BaseTable :loading="isLoading" :process="isProcess" :headers="bannerHeaders" :items="banners" :pagination="pages"
+      @pageChange="handlePageChange">
+      <!-- images -->
+      <template #image="{ row }">
+        <img :src="row.image" :alt="row.name" class="w-40 h-20 rounded object-cover">
+      </template>
+
+      <!-- periode -->
+      <template #periode="{ row }">
+        <span>{{ moment(row.startDate).format('DD MMM YYYY') }} - {{ moment(row.endDate).format('DD MMM YYYY') }}</span>
+      </template>
+
+      <!-- actions -->
+      <template #actions="{ row, index }">
+        <div :key="index" class="py-1 space-x-2 items-center flex justify-end">
+          <BaseAction type="edit" @onAction="handleEdit(row)" />
+          <BaseAction type="delete" @onAction="handleDelete(row)" />
+        </div>
+      </template>
+    </BaseTable>
+
+    <!-- Drawer -->
+    <Drawer v-model="isDrawerOpen" :title="`${isEdit ? 'Edit' : 'Add'} Banner`">
+      <form @submit.prevent="handleSave" class="flex flex-col h-full justify-between">
+        <!-- <div class="overflow-y-auto p-4 px-6"> -->
+        <div class=" p-4 px-6">
+          <!-- Browe Upload -->
+          <BaseUpload v-model="form.image" accepted=".jpg, .jpeg, .png" class=" mb-4" />
+
+          <!-- Input Fields -->
+          <div class="space-y-3">
+            <BaseInput v-model="form.name" placeholder="Name" />
+            <BaseSelect v-mode="form.type" :options="typeOptions" placeholder="Type" />
+            <DateRangePicker v-model="form.dateRange" />
+          </div>
+        </div>
+
+        <!-- Footer Button -->
+        <div class="border-t border-[#e7e8e7] p-4 px-6 mt-auto flex justify-start gap-3">
+          <BaseButton type="submit" variant="filled">
+            Save
+          </BaseButton>
+          <BaseButton @click="cancelForm" variant="outline">
+            Cancel
+          </BaseButton>
+        </div>
+      </form>
+    </Drawer>
+  </div>
+</template>
