18de09b0a4146f1211f50bb06f3f8473024d18d9
taufik <taufik@codelabs.co.id>
2025-11-27 09:41:40 +0700

    update: handle set value to file show

--- src/components/BaseUpload.vue
+++ src/components/BaseUpload.vue
@@ -1,18 +1,45 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { toast } from "vue-sonner"
 
 const props = defineProps({
+  modelValue: {
+    type: String,
+    default: null
+  },
   accepted: {
     type: String,
     default: '.jpg,.jpeg,.png'
   }
 })
 
-const emit = defineEmits(['update:file'])
+const emit = defineEmits(['update:modelValue'])
 const file = ref(null)
 const preview = ref(null)
 const isDragging = ref(false)
+
+watch(
+  () => props.modelValue,
+  (val) => {
+    if (!val) {
+      file.value = null
+      preview.value = null
+      return
+    }
+
+    if (typeof val === "string") {      
+      file.value = null
+      preview.value = val
+      return
+    }
+
+    if (val instanceof File) {
+      file.value = val
+      preview.value = URL.createObjectURL(val)
+    }
+  },
+  { immediate: true }
+)
 
 const handleDrop = (e) => {
   e.preventDefault()
@@ -29,13 +56,13 @@ const handleFileSelect = (e) => {
 const setFile = (f) => {
   file.value = f
   preview.value = URL.createObjectURL(f)
-  emit('update:file', f)
+  emit('update:modelValue', f)
 }
 
 const removeFile = () => {
   file.value = null
   preview.value = null
-  emit('update:file', null)
+  emit('update:modelValue', null)
 }
 
 const validateFile2 = (f, accept) => {
@@ -55,7 +82,6 @@ const validateFile2 = (f, accept) => {
 
   return true
 }
-
 
 const validateFile = (f, accept = '.jpg,.jpeg,.png', maxSizeMB = 2) => {
   const allowedTypes = accept.split(',').map(t => t.trim()) || ['.jpg', '.jpeg', '.png']
@@ -92,7 +118,7 @@ const validateFile = (f, accept = '.jpg,.jpeg,.png', maxSizeMB = 2) => {
       :class="isDragging ? 'border-blue-400 bg-blue-50' : 'border-gray-300 hover:border-blue-300'"
       @dragover.prevent="isDragging = true" @dragleave.prevent="isDragging = false" @drop="handleDrop">
       <!-- If not yet -->
-      <div v-if="!file">
+      <div v-if="!file && !preview">
         <p class="text-gray-600 text-sm">Drag your image to start uploading</p>
         <p class="text-gray-400 my-1 text-sm">or</p>
         <label
@@ -104,9 +130,11 @@ const validateFile = (f, accept = '.jpg,.jpeg,.png', maxSizeMB = 2) => {
 
       <!-- If file exist -->
       <div v-else class="flex flex-col items-center space-y-3">
-        <img v-if="['image/jpeg', 'image/png'].includes(file.type)" :src="preview" alt="preview"
+        <img v-if="['image/jpeg', 'image/png'].includes(file?.type)" :src="preview" alt="preview"
           class="max-h-48 rounded-md shadow-md object-contain" />
-        <p class="text-sm text-gray-700 font-medium">{{ file.name }}</p>
+        <img v-else :src="preview" alt="preview"
+          class="max-h-48 rounded-md shadow-md object-contain" />
+        <p class="text-sm text-gray-700 font-medium">{{ file?.name }}</p>
         <button class="px-3 py-1 text-sm bg-red-100 text-red-600 rounded hover:bg-red-200 transition cursor-pointer"
           @click="removeFile">
           Remove
