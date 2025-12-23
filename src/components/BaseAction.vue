524618f8ec81b27df43f6bf144a9e4150a793469
taufik <taufik@codelabs.co.id>
2025-12-23 19:28:05 +0700

    update: handle input on enter

--- src/components/BaseAction.vue
+++ src/components/BaseAction.vue
@@ -13,13 +13,13 @@ defineProps({
     @click.stop="$emit('onAction')"
     :class="[
       'p-2 rounded-md transition cursor-pointer',
-      type === 'edit'
+      type === 'edit' || type === 'view'
         ? 'text-blue-600 bg-blue-50 hover:bg-blue-100'
         : 'text-red-600 bg-red-50 hover:bg-red-100'
     ]"
   >
     <Icon
-      :icon="type === 'edit' ? 'mdi:pencil-outline' : 'mdi:trash-can-outline'"
+      :icon="type === 'edit' ? 'mdi:pencil-outline' : type === 'delete' ? 'mdi:trash-can-outline' : 'mdi:eye-outline'"
       class="w-4 h-4"
     />
   </button>
